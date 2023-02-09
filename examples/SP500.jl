fe4279e7348b5b4f101d7fb0cc0b1cd5fac316c1
PharosAbad <pharos.abad@gmail.com>
2023-02-09 20:50:40 +0800

    main

--- examples/SP500.jl
+++ examples/SP500.jl
@@ -5,35 +5,42 @@ using LightenQP
 
 using TranscodingStreams, CodecXz, Serialization, Downloads
 
-#download the online data
-xzFile = "/tmp/sp500.jls.xz"
-if !isfile(xzFile)
-    Downloads.download("https://github.com/PharosAbad/PharosAbad.github.io/raw/master/files/sp500.jls.xz", xzFile)
+function main()
+
+    #download the online data
+    xzFile = "/tmp/sp500.jls.xz"
+    if !isfile(xzFile)
+        Downloads.download("https://github.com/PharosAbad/PharosAbad.github.io/raw/master/files/sp500.jls.xz", xzFile)
+    end
+    io = open(xzFile)
+    io = TranscodingStream(XzDecompressor(), io)
+    E = deserialize(io)
+    V = deserialize(io)
+    close(io)
+
+    N = length(E)
+    u = fill(3 / 32, N)
+
+    #= Q = OOQP(V, zeros(N), u)
+    ts = @elapsed x, status = solveOOQP(Q)
+    println("--- LightenQP Method @LVEP (Lowest Variance Efficient Portfolio, also called GMVP, Global Minimum Variance Portfolio):  ", ts, "  seconds")
+
+    Q1 = OOQP(V, -E, u)
+    ts = @elapsed x1, status1 = solveOOQP(Q1)
+    println("--- LightenQP Method @L=1:  ", ts, "  seconds")    =#
+
+    #v1.0.1
+    Q = OOQP(V, E, u)
+    ts = @elapsed xH, statusH = fPortfolio(Q; L=Inf)       #HMFP (Highest Mean Frontier Portfolio)
+    println("HMFP (Highest Mean Frontier Portfolio) --- fPortfolio @ max mu:  ", ts, "  seconds")
+    ts = @elapsed xL, statusL = fPortfolio(Q; L=-Inf)      #LMFP (Lowest Mean Frontier Portfolio)
+    println("LMFP (Lowest Mean Frontier Portfolio)  --- fPortfolio @ min mu:  ", ts, "  seconds")
+    ts = @elapsed x0, status0 = fPortfolio(Q; L=0.0)
+    println("LVEP (Lowest Variance Efficient Portfolio, also called GMVP, Global Minimum Variance Portfolio) --- fPortfolio @ L=0:  ", ts, "  seconds")
 end
-io = open(xzFile)
-io = TranscodingStream(XzDecompressor(), io)
-E = deserialize(io)
-V = deserialize(io)
-close(io)
 
-N = length(E)
-u = fill(3 / 32, N)
+main()
+nothing
 
-#= Q = OOQP(V, zeros(N), u)
-ts = @elapsed x, status = solveOOQP(Q)
-println("--- LightenQP Method @LVEP (Lowest Variance Efficient Portfolio, also called GMVP, Global Minimum Variance Portfolio):  ", ts, "  seconds")
-
-Q1 = OOQP(V, -E, u)
-ts = @elapsed x1, status1 = solveOOQP(Q1)
-println("--- LightenQP Method @L=1:  ", ts, "  seconds")    =#
-
-#v1.0.1
-Q = OOQP(V, E, u)
-ts = @elapsed xH, statusH = fPortfolio(Q; L=Inf)       #HMFP (Highest Mean Frontier Portfolio)
-println("HMFP (Highest Mean Frontier Portfolio) --- fPortfolio @ max mu:  ", ts, "  seconds")
-ts = @elapsed xL, statusL = fPortfolio(Q; L=-Inf)      #LMFP (Lowest Mean Frontier Portfolio)
-println("LMFP (Lowest Mean Frontier Portfolio)  --- fPortfolio @ min mu:  ", ts, "  seconds")
-ts = @elapsed x0, status0 = fPortfolio(Q; L=0.0)
-println("LVEP (Lowest Variance Efficient Portfolio, also called GMVP, Global Minimum Variance Portfolio) --- fPortfolio @ L=0:  ", ts, "  seconds")
 
 
--- examples/portfolio.jl
+++ examples/portfolio.jl
@@ -8,26 +8,26 @@
 using LightenQP
 using LinearAlgebra
 
-V = [1/100 1/80 1/100
-     1/80 1/16 1/40
-     1/100 1/40 1/25]
-E = [109 / 100; 23 / 20; 119 / 100]
+function main()
 
-#Q = OOQP(V, -E)
-#x, status = solveOOQP(Q)
+     V = [1/100 1/80 1/100
+          1/80 1/16 1/40
+          1/100 1/40 1/25]
+     E = [109 / 100; 23 / 20; 119 / 100]
 
+     #Q = OOQP(V, -E)
+     #x, status = solveOOQP(Q)
 
+     u = [0.7; +Inf; 0.7]     #Inf means no bounded
+     Q = OOQP(V, -E, u)       #OOQP + bounds 0 <= x <= u
+     #settings = Settings()
+     x, status = solveOOQP(Q) #solve by Algorithm MPC (Mehrotra Predictor-Corrector)
 
-u = [0.7; +Inf; 0.7]     #Inf means no bounded
-Q = OOQP(V, -E, u)       #OOQP + bounds 0 <= x <= u
-#settings = Settings()
-x, status = solveOOQP(Q) #solve by Algorithm MPC (Mehrotra Predictor-Corrector)
-
-#v1.0.1
-O = OOQP(V, E, u)
-y, statusy = fPortfolio(O; L=1.0)
-norm(x.x-y.x, Inf)  #0
-
+     #v1.0.1
+     O = OOQP(V, E, u)
+     y, statusy = fPortfolio(O; L=1.0)
+     display(norm(x.x - y.x, Inf))  #0
+end
 
 
 #=
@@ -42,5 +42,5 @@ z = ePortfolio(aEF, mu)
 x.x - z
 =#
 
-
-
+main()
+nothing
